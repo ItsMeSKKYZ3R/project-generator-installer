2b034ce613e139058088d17a94d2177bc2e69486
ItsMeSKKYZ3R <71552277+ItsMeSKKYZ3R@users.noreply.github.com>
2022-07-04 23:21:47 +0200

    Start to create Linux support

--- src/file.rs
+++ src/file.rs
@@ -11,10 +11,11 @@ error_chain! {
      }
 }
 
+#[cfg(target_os = "windows")]
 pub async fn download() -> Result<()> {
     let tmp_dir = PathBuf::new();
-    let target = "https://skkyz3r.fr/password-generator.exe";
-    let response = reqwest::get(target).await?;
+    let target = "https://skkyz3r.fr/project-generator.exe";
+    let response = reqwest::get(target).await.expect("Failed to execute request.");
 
     let mut dest = {
         let fname = response
@@ -27,10 +28,39 @@ pub async fn download() -> Result<()> {
         println!("file to download: '{}'", fname);
         let fname = tmp_dir.join("Program Files").join("Project Generator").join(fname);
         println!("will be located under: '{:?}'", fname);
-        File::create(fname)?
+        File::create(fname).expect("Failed to create file")
     };
-    let content =  response.text().await?;
-    copy(&mut content.as_bytes(), &mut dest)?;
+    let content =  response.text().await.expect("Failed to get response text.");
+    copy(&mut content.as_bytes(), &mut dest).expect("Failed to copy file to destination.");
+
+    Ok(())
+}
+
+#[cfg(target_os = "linux")]
+pub async fn download() -> Result<()> {
+    let dir = dirs::executable_dir().expect("Failed to get executable files directory");
+    println!("{dir:?}");
+    let target = "https://skkyz3r.fr/project-generator";
+    let response = reqwest::get(target).await.expect("Failed to execute request.");
+
+    let mut dest = {
+        let fname = response
+            .url()
+            .path_segments()
+            .and_then(|segments| segments.last())
+            .and_then(|name| if name.is_empty() { None } else { Some(name) })
+            .unwrap_or("tmp.bin");
+
+        println!("File to download: '{fname}'");
+
+        let fname = dir.join(fname);
+        println!("Will be located under: '{fname:?}'");
+
+        File::create(fname).expect("Failed to create file")
+    };
+
+    let content = response.text().await.expect("Failed to get response text.");
+    copy(&mut content.as_bytes(), &mut dest).expect("Failed to copy file to destination.");
 
     Ok(())
 }
